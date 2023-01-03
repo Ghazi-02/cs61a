f70ac15d27a1249084430fb6c30dab1b93e6c2d9
Areeb <auhmirza02@gmail.com>
2023-01-03 12:30:03 +0000

    Implement procedure ascending? checks for ascending lists

--- homework/hw07/hw07.scm
+++ homework/hw07/hw07.scm
@@ -4,8 +4,16 @@
 
 (define (caddr s) (car (cdr (cdr s))))
 
-(define (ascending? asc-lst) 'YOUR-CODE-HERE)
+(define (ascending? asc-lst) 
 
+    (cond 
+        ((null? (cdr asc-lst))
+            #t)
+        (( > (car asc-lst)(cadr asc-lst))
+            #f )            
+        (else
+            (ascending?(cdr asc-lst)))
+        ))
 (define (square n) (* n n))
 
 (define (pow base exp) 'YOUR-CODE-HERE)
