7fbac961fe2d9ef650a9351b01fbd7bb5b7365a2
Areeb <auhmirza02@gmail.com>
2023-01-03 11:59:54 +0000

    disc and hw07

---
+++ homework/hw07/hw07.scm
@@ -0,0 +1,11 @@
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) (car (cdr s)))
+
+(define (caddr s) (car (cdr (cdr s))))
+
+(define (ascending? asc-lst) 'YOUR-CODE-HERE)
+
+(define (square n) (* n n))
+
+(define (pow base exp) 'YOUR-CODE-HERE)
