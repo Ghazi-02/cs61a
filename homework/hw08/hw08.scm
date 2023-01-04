d7f115b52750d3ef0a9be6bcd6c495a6c3717d4e
Areeb <auhmirza02@gmail.com>
2023-01-04 15:18:58 +0000

    add my filter procedure in scheme

---
+++ homework/hw08/hw08.scm
@@ -0,0 +1,16 @@
+(define (my-filter pred s) 
+    
+    (cond 
+        ((null? s ) s)
+        ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+        (else (my-filter pred (cdr s)))
+    )
+    
+)
+
+(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+
+(define (accumulate joiner start n term)
+  'YOUR-CODE-HERE)
+
+(define (no-repeats lst) 'YOUR-CODE-HERE)
