77f0de64d13f8703d6666c88bb249398b9d35bc0
Areeb <auhmirza02@gmail.com>
2023-01-04 11:58:20 +0000

    scheme map function

--- disc/scm.scm
+++ disc/scm.scm
@@ -22,5 +22,15 @@
         )
     )
 )    
-
+(define (map-fn fn lst)
+        (cond 
+            ((null? lst) lst)
+    
+    
+            (else  (cons (fn (car lst))  (map-fn fn (cdr lst)))
+            )
+        )
+)
+(map-fn (lambda (x) (* x x)) '(1 2 3))
+; expect (1 4 9)
     
