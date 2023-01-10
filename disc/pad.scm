be7a329a245c51764187dfce6ea5a2878d7230ce
Areeb <auhmirza02@gmail.com>
2023-01-10 12:15:17 +0000

    add pow-expr procedure in pad.scm

--- disc/pad.scm
+++ disc/pad.scm
@@ -5,3 +5,38 @@
 (expect (if-program '(= 1 0) '(print 3) '(print 5)) (if (= 1 0) (print 3) (print 5)))
 (expect (eval (if-program '(= 0 0) '2 '3)) 2)
 (expect (eval (if-program '(= 1 0) '(print 3) '(print 5))) 5)
+
+(define (pow n p) ; just pow procedure 
+  (define (helper m q)
+    `(cond
+        ,((= q 1) m)
+        (else
+            ,(helper (* n m) (- q 1)))
+    )
+  )
+    (helper n p) 
+
+)
+
+(define (pow-expr base exp) 
+        (cond
+            ((= exp 0)
+                1)
+            (else
+                  `(* ,(pow-expr base (- exp 1)) ,base))
+            
+    
+        )
+
+)
+
+(define expr (pow-expr 5 1))
+(define expr2 (pow-expr 5 2))
+(expect expr
+(* 1 5))
+(expect (eval expr)
+5)
+(expect expr2 
+(* (* 1 5) 5))
+(expect (eval expr2)
+25)
