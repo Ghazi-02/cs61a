e4345c0f05e17cf492245b151dfb6a1e3e88aa9c
Areeb <auhmirza02@gmail.com>
2023-01-11 13:13:12 +0000

    Define reverse list tail call procedure in m&tc

--- disc/m&tc.scm
+++ disc/m&tc.scm
@@ -13,6 +13,7 @@
       (replace-helper (cdr e ) o n)))
 (define-macro (replace expr old new)
     (replace-helper expr old new))
+
 (define (sum lst)
   (define (sumAll lst total)
     (cond 
@@ -24,3 +25,18 @@
 )
 (expect (sum '(1 2 3)) 6)
 (expect (sum '(10 -3 4)) 11)
+
+(define (reverse lst)
+  (define (helper lst result length )
+    (cond
+       
+        ((= length 0) result) 
+        (else (helper  (cdr lst) (cons (car lst) result) (- length 1)))
+        )
+  )
+  (helper lst nil (length lst))
+)
+
+(expect (reverse '(1 2 3)) (3 2 1))
+(expect (reverse '(0 9 1 2)) (2 1 9 0))
+
